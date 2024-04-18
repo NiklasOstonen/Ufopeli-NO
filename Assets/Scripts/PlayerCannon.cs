980b23b615439c941855c77e7c9f0d3eb4a6fd66
NiklasOstonen <s2101042@edu.bc.fi>
2024-04-18 11:02:39 +0300

    Pause
    Pelin voi laittaa tauko tilaan

--- Assets/Scripts/PlayerCannon.cs
+++ Assets/Scripts/PlayerCannon.cs
@@ -3,33 +3,38 @@ using UnityEngine;
 
 public class PlayerCannon : MonoBehaviour
 {
-    float mx;
-    public float movementSpeed = 5f;
+    public int speed;
     Rigidbody2D rb;
     public EventHandler playerDied;
-
-
-    // public float moveSpeed = 10;
-    // public float hInput;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        speed = 100;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        mx = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0)
+        {
+            float turnSpeed = speed * Time.deltaTime;
 
-    }
+            //Connect turning rate to horizonal motion for smooth transition
+            float rotate = Input.GetAxis("Horizontal") * turnSpeed;
 
-    private void FixedUpdate()
-    {
-        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
-        rb.velocity = movement;
+            //Get current rotation
+            float currentRotation = gameObject.transform.rotation.z;
+
+            //Add current rotation to rotation rate to get new rotation
+            Quaternion rotation = Quaternion.Euler(0, 0, currentRotation + rotate);
+
+            //Move object to new rotation
+            gameObject.transform.rotation = rotation;
+        }
+
     }
 
 }
